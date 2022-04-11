dbc25390bde843f6f53b266a5fcb7b5d7fe84795
Yufri Luttu <81541818+yufri28@users.noreply.github.com>
2022-04-11 09:30:19 +0800

    Update Program.cs

--- ReadTextFile/Program.cs
+++ ReadTextFile/Program.cs
@@ -13,6 +13,7 @@ namespace ReadTextFile
        
         static void Main(string[] args)
         {
+            //bisa menggunakan ReadAllText (Baca semua text) atau ReadAllLines (baca semua baris)
             string filePath = @"C:\Users\USER\Desktop\ReadFiles.txt"; //Path File atau jalur penyimpanan file.txt (pada cth, ReadFiles.txt/file .txtnya disimpan pada C:\Users\USER\Desktop\
             string lines = File.ReadAllText(filePath); //panggil method ReadAllText() atau method untuk membaca isi file dalam ReadFiles.txt dengan menyertakan parameter filePath dan disimpan dalam variabel lines. ReadAllText untuk membaca semua text yang ada di dalam file ReadFiles.txt
             string[] get = lines.Split(';'); //dari semua teks yang ada dalam file ReadFiles.txt, teks2 tersebut dipisahkan dengan tanda ; dan disimpan menjadi array 1 dimensi.
